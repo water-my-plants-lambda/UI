b71806953828862208ad1b4b9de1d213a86c21d7
Chris Johnston <cmjohnston@gmail.com>
2019-05-31 13:10:00 -0500

    trying to get js to work on contact form

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
-class Menu {
-  constructor(menulist) {
-    // assign this menulist to the passed in menulist
-    this.menulist = menulist;
-    // create a reference to the ".nav-icon" class.
-    this.navopen = this.menulist.querySelector('.nav-icon');
-    // console log this shit
-    console.log('does this shit work');
-    //replace open menu with x
-    this.navopen.textContent = 'x';
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.navopen.addEventListener('click', () => this.toggleNav());
-  }
-}
+signupButton.forEach(node => {
+  node.addEventListener('click', function() {
+    alert(`thank you for contacting us`);
+    console.log('hello I am here');
+  });
+});
 
-const toggleNav = () => {
-  if (this.navopen.textContent === 'x') {
-    this.navopen.textContent = '.navicon';
-  } else {
-    this.expandButton.textContent = 'expand';
-  }
-  this.domElement.classList.toggle('article-open');
-};
+const formButton = document.getElementById('contactBtn');
 
-const menu = document.querySelector('nav-icon');
-// hamburger.addEventListener('click', toggleNav());
+// class Menu {
+//   constructor(menulist) {
+//     // assign this menulist to the passed in menulist
+//     this.menulist = menulist;
+//     // create a reference to the ".nav-icon" class.
+//     this.navopen = this.menulist.querySelector('.nav-icon');
+//     // console log this shit
+//     console.log('does this shit work');
+//     //replace open menu with x
+//     this.navopen.textContent = 'x';
+//     // Set a click handler on the expandButton reference, calling the expandArticle method.
+//     this.navopen.addEventListener('click', () => this.toggleNav());
+//   }
+// }
+
+// const toggleNav = () => {
+//   if (this.navopen.textContent === 'x') {
+//     this.navopen.textContent = '.navicon';
+//   } else {
+//     this.expandButton.textContent = 'expand';
+//   }
+//   this.domElement.classList.toggle('article-open');
+// };
+
+// const menu = document.querySelector('nav-icon');
+// // hamburger.addEventListener('click', toggleNav());
+
+// // function thankYou() {
+// //   alert('Thank you for contacting us');
+// // }
+
+// // document.getElementsByTagName('input').addEventListener('click', thankYou());
+
+// const formButton = document.querySelectorAll('submit');
+// signupButton.forEach(node => {
+//   node.addEventListener('click', function() {
+//     alert(`thank you for contacting us`);
+//   });
+// });
